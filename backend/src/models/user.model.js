eea800dbe9875f20f3738e6c852495f97a01f5c3
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-04-05 18:31:07 +0200

    Add validation for email to model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,6 +4,7 @@ const PASSWORD = 'kdpomq78DWbI2r5L'
 const URI = `mongodb+srv://host-app-user:${PASSWORD}@hero-cluster-b5uws.mongodb.net/test?retryWrites=true`
 
 mongoose.connect(URI)
+const emailValidationPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -13,7 +14,8 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     require: true,
-    unique: true
+    unique: true,
+    match: emailValidationPattern
   },
   username: {
     type: String,
