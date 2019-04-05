5e0cc12428757495d0b5144524c856f07338cf40
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-04-05 16:54:37 +0200

    Add /singin route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 const port = 3000
+const userRoute = require('./user')
 
+app.use(userRoute)
 app.get('/', (req, res) => res.send('It works'))
 app.listen(port, () => console.log(`Listening on port ${port}`))
