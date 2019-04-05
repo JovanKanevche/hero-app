8643d1970a2fb49b65f91af3ddf75959b92f7c38
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-04-05 18:23:48 +0200

    Add register functionality

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,33 @@
+const UserModel = require('../models/user.model')
 const express = require('express')
 const router = express.Router()
-
+const bcrypt = require('bcrypt')
 router.get('/user', (req, res) => res.send('This is the /user route'))
 
 router.get('/user/register', (req, res) => {
   const { name, username, email, password } = req.query
-  res.send(`${name} ${username} ${email} ${password}`)
+
+  bcrypt
+    .hash(password, 10)
+    .then(hashed => {
+      const model = new UserModel({
+        name,
+        username,
+        email,
+        password: hashed
+      })
+
+      model
+        .save()
+        .then(doc => {
+          if (!doc || doc.length === 0) return res.status(500).send(doc)
+          res.status(201).send(doc)
+        })
+        .catch(err => res.status(500).json(err))
+    })
+    .catch(err => {
+      req.status(500).json(err)
+    })
 })
 
 router.get('/user/login', (req, res) => {
