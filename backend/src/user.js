d664c4ce5225744384e9006289b6b4a89933313e
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-04-05 17:05:15 +0200

    Set up api routes for the user model

--- backend/src/user.js
+++ backend/src/user.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/signin', (req, res) => res.send('HELLO'))
+router.get('/user', (req, res) => res.send('This is the /user route'))
+
+router.get('/user/register', (req, res) => {
+  const { name, username, email, password } = req.query
+  res.send(`${name} ${username} ${email} ${password}`)
+})
+
+router.get('/user/login', (req, res) => {
+  const { username, password } = req.query
+  res.send(`${username} ${password}`)
+})
+
+router.get('/user/logout', (req, res) => {
+  const { userId, token } = req.query
+  res.send(userId)
+})
 
 module.exports = router
