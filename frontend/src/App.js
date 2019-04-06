5fcb0a2b1c16ef9a8d3940590e59018023492844
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-04-06 21:12:23 +0200

    Add AuthContext to application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
+import React from 'react'
 import './App.css'
 import RegisterScreen from './screens/RegisterScreen'
+import AuthContext from './context/AuthContext'
 
-const App = RegisterScreen
+function App() {
+  const [auth, setAuth] = useState({
+    token: '',
+    loggein: false
+  })
 
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      <RegisterScreen />
+    </AuthContext.Provider>
+  )
+}
 export default App
