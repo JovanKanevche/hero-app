0b07a10befed4734178710a1f573bb1b41468d89
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-04-08 00:07:46 +0200

    Connect frontend to backend via websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,25 @@
 const express = require('express')
-
 const app = express()
 const port = 3001
 const userRoute = require('./routes/user')
+
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 3002 })
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(message)
+    ws.send(
+      JSON.stringify({
+        person: 'JOV',
+        message: 'LOREM IPSUM'
+      })
+    )
+  })
+})
+
+module.exports = wss
 
 app.use(userRoute)
 app.get('/', (req, res) => res.send('It works'))
--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,9 +1,68 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
+import Input from '../components/Input'
+import Button from '../components/Button'
 
-function DashboardScreen() {
-  const [messages, setMessages] = useState([])
+const SOCKET_URL = 'ws://localhost:3002/'
 
-  return <h1>DASHBOARD</h1>
+class DashboardScreen extends Component {
+  socket
+  state = {
+    messages: [],
+    currentMessage: ''
+  }
+
+  setMessages = messages =>
+    this.setState({
+      messages
+    })
+
+  componentDidMount() {
+    this.socket = new WebSocket(SOCKET_URL)
+    this.socket.onmessage = messages =>
+      this.setMessages([...this.state.messages, messages.data])
+  }
+
+  sendMessage = ({ token, message, username }) => {
+    this.socket.send(
+      JSON.stringify({
+        token,
+        message,
+        username
+      })
+    )
+  }
+
+  render() {
+    const { messages, currentMessage } = this.state
+
+    return (
+      <div style={{ maxWidth: '400px' }}>
+        <h1>DASHBOARD</h1>
+        {messages}
+
+        <div>
+          <Input
+            placeholder="your message"
+            onChange={e => {
+              this.setState({
+                currentMessage: e.target.value
+              })
+            }}
+          />
+          <Button
+            style={{ width: '100%' }}
+            onClick={() => {
+              this.sendMessage({
+                message: currentMessage
+              })
+            }}
+          >
+            Send
+          </Button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default DashboardScreen
